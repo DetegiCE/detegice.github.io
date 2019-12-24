374551f63b5edde05024eead52582134abef89e4
DetegiCE <martinok1103@naver.com>
2019-12-25 02:03:25 +0900

    Create 2019-12-25-뒤집혀진 정수.java

---
+++ _posts/assignment/2019r2/cose102/2019-12-25-뒤집혀진 정수.java
@@ -0,0 +1,39 @@
+---
+title: "뒤집혀진 정수"
+date: 2019-12-25 02:02:02
+categories: java ku
+---
+
+# DESCRIPTION
+정수 I를 입력받아 역순으로 출력하는 프로그램을 작성하세요. 예를 들어, reverse(3456)는 6543으로 출력되어야 합니다. 330은 033으로 출력되어야 합니다.
+
+Write a test program that prompts the user to enter an integer I and displays its reversal. For example, reverse(3456) displays 6543. Also, reverse(330) displays 033.
+
+# INPUT
+* Line 1 : 테스트케이스 T (1~1,000)
+
+* Line 2 ~ T+1 : 정수 (1~1,000,000)
+
+ 
+
+# OUTPUT
+* Line 1 ~ T : 뒤집혀진 숫자
+
+ 
+
+# SAMPLE INPUT
+```
+3
+22
+3215
+111011
+```
+
+# SAMPLE OUTPUT
+```
+22
+5123
+110111
+```
+
+<script src="https://gist.github.com/DetegiCE/c3415deef9c1e8b60211c1431e78a89a.js"></script>
